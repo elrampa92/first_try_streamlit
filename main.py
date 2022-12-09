250e57bfd62fd6922a3aab71996862e118a35701
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2022-12-09 21:52:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import pandas as pd
-st.write("vediamo se vaaa")
+st.write("PROVA RPE")
 
-st.write("proviamo")
+number = st.number_input('Insert a number')
+st.write('The current number is ', number)
 
-st.write("Here's our first attempt at using data to create a table:")
+
+'''st.write("Here's our first attempt at using data to create a table:")
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 })
 st.write(df)
 
-st.bar_chart(df)
+st.bar_chart(df)'''
