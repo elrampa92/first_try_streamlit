3d86c6b23f44414a022eda95d675cea1826be1a9
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2022-12-09 21:53:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ number = st.number_input('Insert a number')
 st.write('The current number is ', number)
 
 
-'''st.write("Here's our first attempt at using data to create a table:")
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
-st.write(df)
-
-st.bar_chart(df)'''
+#st.write("Here's our first attempt at using data to create a table:")
+#df = pd.DataFrame({
+    #'first column': [1, 2, 3, 4],
+    #'second column': [10, 20, 30, 40]
+#})
+#st.write(df)
+#st.bar_chart(df)'''
