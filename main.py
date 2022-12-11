360fa3984982b091bbf19e32df7a3844602555b5
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2022-12-11 16:20:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,77 @@
 import streamlit as st
 import pandas as pd
-st.write("PROVA RPE")
+from datetime import date
+import os
 
 
+today = date.today()
+# dd/mm/YY
+t_date = today.strftime("%d/%m/%Y")
+
+#df_excel = pd.DataFrame(columns=['Player', 'Data', 'RPE'])
+
+os.makedirs('/Users/elrampa/Desktop/prova_streamlit/data', exist_ok=True)
+#df_excel.to_excel('/Users/elrampa/Desktop/prova_streamlit/data/DF_RPE.xlsx', index = False)
+
+
+#df["data"] = pd.to_datetime(df.data)
+#df['data'] = df['data'].dt.strftime('%m/%d/%Y')
+
+
+st.title("RPE PAGE")
 from PIL import Image
-image = Image.open("https://github.com/elrampa92/first_try_streamlit/blob/3f023c9ae83765254772d058c2fa1615add9b696/VFC_LOGO.png")
-st.image(image)
-
-number = st.number_input('Insert a number')
-st.write('The current number is ', number)
+image = Image.open('/Users/elrampa/Desktop/prova_streamlit/VFC_LOGO.png')
+st.image(image, width=130)
+st.text("Insert your RPE value")
 
 
-#st.write("Here's our first attempt at using data to create a table:")
-#df = pd.DataFrame({
-    #'first column': [1, 2, 3, 4],
-    #'second column': [10, 20, 30, 40]
-#})
-#st.write(df)
-#st.bar_chart(df)'''
+#col1, col2, col3 = st.columns(3)
+
+
+sel_player = st.selectbox(
+        "Who are you?",
+        ('BADOUIN NOAH','BORECKI KRYSTIAN','BOUDRI AMIN','BUSATO LORENZO','CAMBER DAVID','CAMOLESE NICOLA',
+         'DA POZZO LORENZO','ENEM JAY','IVARSSON ISAK','JONSSON KRISTOFER','KARAGIANNIDIS GEORGIOS',
+         'KYVIK LEO', 'MAGNUSSON ARON','MAKADJI MORRE','MIKAELSSON HILMIR','MOZZO FILIPPO','OKORO ALVIN OBINHA',
+         'REMY MELVIN','RODRIGUES ALVES JAMES','SALVADOR TOMMASO','SANDBERG GABRIEL','SLOWIKOWSKI ERIK',
+         'SPERANDIO TOMMASO','VELCEA VALENTIN')
+)
+sel_date = st.date_input(
+        "Training session date",)
+sel_session = st.radio(
+        "Select the session",
+        options=["Morning", "Afternoon"],
+)
+st.write("Today is: ",t_date)
+
+
+rpe = st.selectbox(
+        'Rate effort of the session',
+        (0, 0.3, 0.5, 0.7, 1, 1.5, 2, 2.5, 3, 4, 5, 6, 7, 8, 9, 10, 11), index = 8)
+#    rpe = st.slider('Rate effort of the session', 0, 10, 3)
+#    st.write("RPE value",rpe)
+b_confirm = st.button(label ='Confirm!')
+if b_confirm:
+    #st.write(sel_player,"- RPE:", rpe,"during ",sel_session," session on", sel_date)
+    new_row = {'Player':sel_player, 'Data':f'{sel_date} - {sel_session}', 'RPE':rpe}
+    #st.write(new_row)
+    df_rpe = pd.read_excel('/Users/elrampa/Desktop/prova_streamlit/data/DF_RPE.xlsx')
+    df_rpe = df_rpe.append(new_row, ignore_index=True)
+    df_rpe.to_excel('/Users/elrampa/Desktop/prova_streamlit/data/DF_RPE.xlsx', index = False)
+    #df_RPE = pd.read_csv('/Users/elrampa/Desktop/prova_streamlit/data/DF_RPE.csv')
+    #df_RPE = df_RPE.append(new_row, ignore_index=True)
+    #df_RPE.to_csv('/Users/elrampa/Desktop/prova_streamlit/data/DF_RPE.csv', index = False)
+    #st.write(df_RPE)
+
+
+
+    #df_rpe.insert(f"{sel_date}_{sel_session}")
+
+    #df['gioc1'][0] = rpe
+
+#print(df_rpe.loc[0])
+#st.write(df_rpe)
+#print(sel_player," ", sel_date, "", rpe, "", sel_session)
+
+#print(df)
+
